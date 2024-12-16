17f59da2413424e17d26443d5b2c2715864a8f0a
daniar <furuichitakayuki@mail.ru>
2024-12-16 16:06:44 +0600

    лаба 3 обнова

--- django-ospanov-1/lab1/users/urls.py
+++ django-ospanov-1/lab1/users/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
 
+app_name = 'users'
 
 urlpatterns = [
-  path('register/', views.register_view, name='list'),
+  path('register/', views.register_view, name='register'),
   path('login/', views.login_view, name="login"),
 ]
